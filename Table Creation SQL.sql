7ac3a1dd498347d2af463749cc6a7fc106b3b3e4
swshover <135381688+swshover@users.noreply.github.com>
2023-08-15 21:06:29 -0500

    done

---
+++ Table Creation SQL.sql
@@ -0,0 +1,106 @@
+-- dropping in case present
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+
+-- TABLE CREATION
+-- employees
+-- Has the 'more important' primary key of emp_no
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY NOT NULL, -- should add UNIQUE here between INT and PRIMARY KEY?
+    emp_title_id VARCHAR(30) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex VARCHAR(30),
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- titles
+-- because employees already has a primay key, titles needs to have a primary key for titles to work.
+CREATE TABLE titles (
+    title_id VARCHAR(30) PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL
+);
+
+-- departments
+CREATE TABLE departments (
+    dept_no VARCHAR(30) PRIMARY KEY NOT NULL,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+-- dept_emp
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(30) NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- dept_manager
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(30) NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- salaries
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- HOMEWORK
+-- 1. List the employee number, last name, first name, sex, and salary of each employee
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
+
+-- 2. List the first name, last name, and hire date for the employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+
+-- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name AS manager_last_name, e.first_name AS manager_first_name
+FROM departments d
+JOIN dept_manager dm ON d.dept_no = dm.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+-- 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_no, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
+
+-- 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B 
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- 6. List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+-- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+-- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name)
+SELECT last_name, COUNT(last_name) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
+
